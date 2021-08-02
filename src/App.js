be4a8400e9e8834d92f9d303f4b0fe7f8a72d6dc
Nghia Nguyen Huu <nhnghia2104@gmail.com>
2021-08-02 23:12:09 +0700

    update logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
 import "./App.css";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import { Layout } from "./components/Container";
-
+const Layout = React.lazy(() => import("./components/Container"));
 function App() {
   return (
     <HashRouter>
